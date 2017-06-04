bd34b4326ab6cab6b8c1d75d7432846ee893e24a
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-06-04 11:41:54 +0200

    Premier style pour le template About

--- App.js
+++ App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import About from './components/About';
 
 export default class App extends React.Component {
     render() {
         return (
-            <View style = { styles.container } >
-                <Text> Open up App.js to start working on your app! </Text>
-                <Text> Changes you make will automatically reload. </Text>
-            </View>
+            <About />
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
--- components/About.js
+++ components/About.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View, Text} from'react-native';
+import {View, Text, StyleSheet} from'react-native';
 
 export default class About extends React.Component {
 
     render() {
         return (
-            <View>
-                <Text>A propos de moi</Text>
+            <View style={style.view}>
+                <Text style={style.title}>A propos de moi</Text>
                 <Text>Je suis développeur et passionné de web</Text>
                 <Text>J'ime apprendre de nouvelles technologies permettent de concevoir des sites ou des applications web et mobile.</Text>
                 <Text>J'ai donc conçu pour vous cette application pour que vos puissiez avoir la météo près de chez vous</Text>
@@ -15,3 +15,13 @@ export default class About extends React.Component {
     }
 
 }
+
+const style = StyleSheet.create({
+    view: {
+        margin: 20
+    },
+    title: {
+        fontSize: 22,
+        marginBottom: 20
+    }
+});
