8167063b6730dbbb1efec966f27824ef896f28df
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-06-04 11:53:40 +0200

    Champ pour indiquer la ville pour laquelle on veut connaitre la météo

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Search from './components/Search';
 import About from './components/About';
+import {View} from'react-native';
 
 export default class App extends React.Component {
     render() {
         return (
-            <About />
+            <View>
+                <Search />
+            </View>
         );
     }
 }
--- components/Search.js
+++ components/Search.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {View, Text, StyleSheetr} from'react-native';
+import {View, TextInput, StyleSheet} from'react-native';
 
 export default class Home extends React.Component {
 
     render() {
         return (
-
+            <View>
+                <TextInput
+                    underlineColorAndroid='transparent'
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                />
+            </View>
         )
     }
 
