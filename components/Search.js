b6add0d2f4844719aba00cab2363b4125d0b6542
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-06-04 11:47:36 +0200

    Création du template Search

---
+++ components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheetr} from'react-native';
+
+export default class Home extends React.Component {
+
+    render() {
+        return (
+
+        )
+    }
+
+}
