ebb10475a01fda8eec81f4a4d60e9b4ebe453a11
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-06-07 18:04:32 +0200

    Navigation

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import {StatusBar, View} from'react-native';
+import {TabNavigator} from 'react-navigation';
 import Search from './components/Search';
 import About from './components/About';
-import {View} from'react-native';
+
+const Tabs = TabNavigator({
+    Search: {screen: Search},
+    About: {screen: About}
+}, {
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+        showIcon: true,
+        showLabel: false,
+        pressColor: "#27ae60"
+    }
+});
 
 export default class App extends React.Component {
     render() {
         return (
-            <View style={{marginVertical: 40}}>
-                <Search />
+            <View style={{flex: 1}}>
+                <StatusBar hidden={true}/>
+                <Tabs />
             </View>
         );
     }
