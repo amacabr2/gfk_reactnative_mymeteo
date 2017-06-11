8c00742f7f2544a6e368ad1cb0585af65b229655
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-06-11 10:02:41 +0200

    Margin entre le boutn recherche et le champ por la ville

--- Style.js
+++ Style.js
@@ -8,7 +8,8 @@ export default StyleSheet.create({
         height: 40,
         borderColor: 'gray',
         borderWidth: 1,
-        padding: 4
+        padding: 4,
+        marginBottom: 20
     },
     button: {
         backgroundColor: '#a2273c',
