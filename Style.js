b946f1c4bd424291cb64ff5bd7ceb363f4273159
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-06-11 10:03:28 +0200

    Padding horizontale pour le champ

--- Style.js
+++ Style.js
@@ -8,7 +8,7 @@ export default StyleSheet.create({
         height: 40,
         borderColor: 'gray',
         borderWidth: 1,
-        padding: 4,
+        paddingHorizontal: 10,
         marginBottom: 20
     },
     button: {
